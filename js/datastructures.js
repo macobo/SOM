1eb43826c972d32fb7639856bd11acc294f0b7f4
macobo <oxymaccy@gmail.com>
2012-12-27 16:50:09 +0200

    Progress bar

--- js/datastructures.js
+++ js/datastructures.js
@@ -2,25 +2,36 @@
 // Should be loaded as paperscript
 
 (function() {
+	// Enum of states
+	var mapRadius = 400;
+	var timeConstant = 300 / Math.log(mapRadius);
+	var learningConstant = 0.2;
+	window.states = Object.freeze({
+		NONE: 0,
+		CREATING_DATA: 1,
+		ITERATING: 2
+	});
+
+	window.pathLayer = new Layer();
 	// Styles, used for configuration
 	// Path
 	pathStyle = {
 	    strokeColor:"#60F",
 	    strokeWidth: 2,
-	    dashArray: [3, 3],
+	    //dashArray: [3, 3],
 	    stokeCap: 'round',
 	    radius: 10
 	};
 	// Circle indicating current position
 	indicatorStyle = {
-	    fillColor: "#F00", 
+	    fillColor: "#FFF", 
 	    strokeColor: "#000",
 	    radius: 10
 	};
 	// circle(s) denoting previous positions.
 	wayPostStyle = {
 		fillColor: "#777", 
-		opacity: 0.8,
+		opacity: 0.3,
 		radius: 3,
 	};
 
@@ -34,18 +45,33 @@
 	function NeuronPath(point) {
 		this.path = new Path();
 		this.path.style = pathStyle;
+		pathLayer.addChild(this.path);
 		this.pathGroup = new Group();
 		this.add(point);
 		this.indicator = new Path.Circle(point, indicatorStyle.radius);
 		this.indicator.style = indicatorStyle;
 	}
 
+	function memoizedPoint(curve, part) {
+		if (curve.cache === undefined) {
+			curve.cache = {};
+		}
+		var x = Number(part * 100);
+		if (!(curve.cache.hasOwnProperty(x))) 
+			curve.cache[x] = curve.getPoint(part);
+		return curve.getPoint(part);
+	}
+
 	// moves the indicator along some segment to part
 	NeuronPath.prototype.moveIndicator = function(segment, part) {
+		if (segment === undefined) {
+			segment = this.segmentCount() - 1;
+			part = 1;
+		}
 		if (this.segmentCount() == 0)
 			return;
 		var curve = this.path.curves[segment % this.segmentCount()];
-		this.indicator.position = curve.getPoint(part);
+		this.indicator.position = memoizedPoint(curve, part);
 	}
 
 	// change the color of the indicator
@@ -64,7 +90,7 @@
 	// adds a point to path
 	NeuronPath.prototype.add = function(point) {
 		point = translate(point);
-		console.log(this, point);
+		//console.log("adding to NeuronPath ",this, point);
 	    this.path.add(point);
 	    this.path.smooth();
 	    // how to move them later
@@ -73,49 +99,225 @@
 	    );
 	}
 
-	// Neuron - datastructure containing the info
-	// startValue - the initial value of the neuron.
-	function Neuron(startValue) {
-		var point = new Point(startValue);
-		this.data = [startValue];
-		this.path = new NeuronPath(point);
+	// Neuron - datastructure containing the info about a neuron
+	function Neuron(startState) {
+		this.data = [];		
+		if (startState !== undefined)
+			this.addState(startState);
 	} 
 
-	// Gives the value of data at the given moment.
+	_.extend(Neuron, {
+		DATA: {
+			indicator: {fillColor: "#FFF"},
+		},
+		DATASELECTED: {
+			indicator: {fillColor: "#00F"},
+			oneHolder: true
+		},
+		NEUTRAL: {
+			indicator: {fillColor: "#090"},
+		},
+		BMU: {
+			indicator: {fillColor: "#F00"},
+			oneHolder: true,
+		},
+		MATCH: {
+			indicator: {fillColor: "#FF0"},
+		},
+	});
+
+	Neuron.setStyles = function(style, prevStyle) {
+		var holders = prevStyle.holders;
+		prevStyle.holders = [];
+		_.each(holders, function(neuron) {
+			neuron.setStyle(style);
+		})
+	}
+
+	Neuron.prototype.setStyle = function(style) {
+		if (style.indicator !== undefined) {
+			_.extend(this.path.indicator.style, style.indicator);
+		}
+		this.prevStyle = style;
+	}
+
+	// Gives a copy of value of data at the given moment.
 	// If no parameter is given, the last datapoint is returned.
 	Neuron.prototype.state = function(moment) {
 		if (moment === undefined)
 			moment = this.data.length-1;
-		return this.data[moment];
+		return this.data[moment].slice();
 	} 
 
-	// Distance (squared) to other neuron
-	Neuron.prototype.distance(neuron) {
+	// Returns the neuron.
+	Neuron.prototype.addState = function(newState) {
+		var point = new Point(newState);
+		if (!Array.isArray(newState))
+			newState = [newState.x, newState.y];
+
+		if (this.path === undefined) {
+			this.path = new NeuronPath(point);
+		} else {
+			this.path.add(point);
+		}
+		this.data.push(newState);
+	}
+
+	// Distance from current state (squared) to other neurons current state
+	Neuron.prototype.distance = function(neuron) {
 		var a = new Point(this.state());
-		var b = new Point(neuron.state());
-		return a.distance(b);
+		if (neuron instanceof Neuron) {
+			var b = new Point(neuron.state());
+			return a.getDistance(b);
+		}
+		return neuron.getDistance(a);
 	}
 
 	// Calculates the influence over distance, with the neighborhood radius being radius.
 	function influence(distance, radius) {
-		return exp(-1.0 * distance * distance / (2 * radius * radius));
+		return Math.exp(-1.0 * distance * distance / (2 * radius * radius));
 	}
 
 	// Calculates the neighborhood radius at given iteration
 	function neighborhoodRadius(iteration, timeConstant) {
-		return mapRadius * exp(-1.0 * iteration / timeConstant);
+		// TODO: real mapRadius implementation?
+		return mapRadius * Math.exp(-1.0 * iteration / timeConstant);
 	}
 
 	// Calculates the learning rate at iteration
-	function learningRate(iteration, learningConstant) {
-		
+	function learningRate(iteration, learningConstant, timeConstant) {
+		return learningConstant * Math.exp(-1.0 * iteration / timeConstant);
 	}
 
+	// Updates the neuron (by adding a new state)
+	Neuron.prototype.update = function(BMU, vector, iteration) {
+		var R = neighborhoodRadius(iteration, timeConstant);
+		var distance = this.distance(BMU);
+		//if (distance > R) return;
+		
+		var L = learningRate(iteration, learningConstant, timeConstant);
+		var I = influence(distance, R);
+		// A prettier way to do it would be nice...
+		var state = this.state(); 
+		if (distance <= R) {
+			this.setStyle(Neuron.MATCH);
+			for (var i = 0; i < vector.length; i++) {
+				state[i] += L * I * (vector[i] - state[i]);
+			}
+
+		}
+		//console.log("Updating ", this, "adding new state ", state);
+		this.addState(state);
+	}
+
+	Neuron.findBMU = function(neurons, vector) {
+		var point = new Point(vector);
+		var closest = _.min(neurons, function(neuron) {
+			//console.log(neuron.data[0], vector, neuron.distance(point));
+			return neuron.distance(point); 
+		});
+		//console.log(point, closest);
+		return closest;
+	}
+
+	Neuron.updateAll = function(neurons, vector, iteration) {
+		var BMU = Neuron.findBMU(neurons, vector);
+		console.log("BMU: ", BMU);
+		_.map(neurons, function(neuron) { 
+			neuron.setStyle(Neuron.NEUTRAL);
+			return neuron.update(BMU, vector, iteration); 
+		});
+		BMU.setStyle(Neuron.BMU);
+	}
+
+	// Generates new neurons within the box created by boxHighLeft, boxLowRight.
+	// If they are not provided, the size of canvas is used.
+	Neuron.generate = function(amount, boxHighLeft, boxLowRight) {
+		if (boxHighLeft === undefined) {
+			boxHighLeft = new Point(0, 0);
+			boxLowRight = new Point(canvas.width, canvas.height);
+		}
+		var size = boxLowRight - boxHighLeft;
+		// perhaps a generator pattern would have been better?
+		var neurons = []
+		for (var i = 0; i < amount; i++) {
+			var point = Point.random() * size + boxHighLeft;
+			neurons.push(new Neuron(point));
+		}
+		return neurons;
+	}
+
+	var infoText = new PointText(new Point(0,20))
+	infoText.characterStyle = {
+	    fontSize: 20,
+	    fillColor: 'black',
+	};
 
 	window.NeuronPath = NeuronPath;
 	window.Neuron = Neuron;
-	console.log("ready");
+	window.infoText = infoText;
 })();
+
+var state = states.CREATING_DATA;
+data = [];
+neurons = [];
+
+tool.minDistance = 10;
+function onMouseDrag(event) {
+	if (state == states.CREATING_DATA) {
+		data.push(new Neuron(event.point));
+	}
+}
+var lastTime = 0, x = 0, target = 0;
+var timeframe = 1;
+function onMouseDown(event) {
+	if (state == states.CREATING_DATA) {
+		data.push(new Neuron(event.point));
+	}
+}
+
+function selectRandom(data) {
+	return data[Math.floor(Math.random() * data.length) ];
+}
+var iteration = 0;
+
+function nextIteration(data, neurons) {
+	_.each(data, function(d) { d.setStyle(Neuron.DATA) });
+	var vector = selectRandom(data);
+	vector.setStyle(Neuron.NEUTRAL);
+	//console.log("Data vector", vector.state());
+	Neuron.updateAll(neurons, vector.state(), iteration);
+	iteration += 1;
+}
+
+function onFrame(event) {
+	if (state == states.ITERATING) {
+		//console.log(event.delta);
+		x = (event.time - lastTime) / timeframe;
+	    if (x >= 1) {
+	        target = (target + 1);
+	        x -= 1;
+	        lastTime = event.time;
+	    }
+	    _.each(neurons, function(neuron) {
+	    	neuron.path.moveIndicator(target, x);
+	    });
+	}
+}
+
+function onKeyDown(event) {
+	if (state == states.CREATING_DATA && data.length > 10) {
+		state = states.ITERATING;
+		neurons = Neuron.generate(109);
+	} else if (state == states.ITERATING) {
+		nextIteration(data, neurons);
+	}
+}
+
+function onMouseMove(event) {
+	infoText.content = ""+event.event.x+" "+event.event.y;
+}
+
 
 /*
 The plan:
@@ -135,11 +337,13 @@ used within logic to coordinates shown in canvas at the moment (used for scaling
 If so, there's a need for a update method which updates the path as needed.
 */
 
+/* 
+// Implementation for points flying around - used for testing
 neurons = [
 	new NeuronPath(new Point(300, 300)), 
 	new NeuronPath(new Point(0,0)),
 	new NeuronPath(new Point(400, 400)),
-	new NeuronPath(new Point(700, 500))
+d	new NeuronPath(new Point(700, 500))
 ];
 //neurons[0].add(new Point(400, 350));
 console.log(neurons);
@@ -153,7 +357,7 @@ function onFrame(event) {
     //console.log(x, event.time, lastTime, neurons[active]);
     if (x >= 1) {
         target = (target + 1);
-        x = 0;
+        x -= 1;
         lastTime = event.time;
     }
     //console.log(neuron, neuron.indicator.position);
@@ -172,3 +376,5 @@ function onKeyDown(event) {
 	active = (active+1) % neurons.length;
 	neurons[active].setColor("#F00");
 }
+
+*/
---
+++ js/progressbar.js
@@ -0,0 +1,160 @@
+var Indicator = function() {
+	var indicator, range = null;
+	var parent, currentPosition;
+
+	function Indicator(parentRectangle) {
+		parent = parentRectangle;
+		currentPosition = parent.topLeft;
+		indicator = new Path.Line(currentPosition,
+									parent.bottomLeft);
+		indicator.style = {
+			strokeWidth: 3,
+			strokeColor: "#F00",
+			strokeCap: "round"
+		}
+	}
+
+	Indicator.prototype.setPosition = function(where) {
+		if (range !== null) {
+			range.remove();
+			range = null;
+		}
+		var target = parent.topLeft + new Point(parent.width * where, 0);
+		indicator.translate(target - currentPosition);
+		currentPosition = target;
+		indicator.visible = true;
+	}
+
+	Indicator.prototype.setRange = function(left, right) {
+		if (range !== null) {
+			range.remove();
+			range = null;
+		}
+		indicator.visible = false;
+		var top = parent.topLeft.clone();
+		top.x += parent.width * left;
+		var bottom = parent.bottomLeft.clone();
+		bottom.x += parent.width * right;
+		var rect = new Rectangle(top, bottom);
+		range = new Path.RoundRectangle(rect, 3);
+		range.style = {
+			strokeWidth: 3,
+			strokeColor: "#F00",
+			strokeCap: "round"
+		}
+	}
+
+	return Indicator;
+}();
+
+loader = function(selector) {
+	function Loader() {
+		this.loadBar = drawLoader();
+		this.center();
+		this.indicator = new Indicator(this.loadBar.rectangle);
+		this.callbacks = {};
+	}
+
+	function drawLoaderBase(top, bottom) {
+		var delta = 13;
+		var radius = 20;
+		var rect = new Rectangle(top+delta, bottom-delta)
+		var path = new Path.Rectangle(rect, radius);
+		path.strokeWidth = 1;
+		path.strokeColor = "#FFF";
+		path.opacity = 0.6;
+		var off = delta + 3;
+		var loadRect = new Rectangle(top+off, bottom-off);
+		loadBar = new Path.Rectangle(loadRect);
+		loadBar.fillColor = "#DDD";
+		loadBar.opacity = 0.8;
+		loadBar.rectangle = loadRect;
+		return loadBar;
+	}
+
+	function drawLoader() {
+		var width = selector.width();
+		var height = selector.height();
+		var rect = new Rectangle(0,0,width,height);
+		var shell = new Path.RoundRectangle(rect, 20);
+		shell.style = {
+			fillColor: "#333",
+		}
+		shell.opacity = 0.6;
+		return drawLoaderBase(rect.topLeft, rect.bottomRight);
+	}
+
+	Loader.prototype.center = function() {
+		$("#progressbar").css({
+			"bottom": ($(window).height() * 0.05),
+			"left": ($(window).width() - selector.width())/2,
+		});
+		var resizeTimer, center = this.center;
+		$(window).resize(function() {
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(center, 100);
+		});
+	}
+
+	// Returns a number between 0 and 1 - the 
+	Loader.prototype.location = function(x) {
+		var where = (x - this.loadBar.rectangle.left) / this.loadBar.rectangle.width;
+		//console.log("loc", x, where);
+		return Math.min(1, Math.max(0, where));
+	}
+
+	Loader.prototype.inverseLocation = function(val) {
+		return this.loadBar.rectangle.left + val * this.loadBar.rectangle.width;
+	}
+
+	Loader.prototype.inBar = function(point) {
+		var top = this.loadBar.rectangle.topLeft-2;
+		var bottom = this.loadBar.rectangle.bottomRight+2;
+		return top.x <= point.x && top.y <= point.y && point.x <= bottom.x && point.y <= bottom.y;
+	}
+
+	Loader.prototype.setPosition = function(point) {
+		if (this.inBar(point)) {
+			var where = this.location(point.x);
+			this.indicator.setPosition(where);
+		}
+	}
+
+	Loader.prototype.setRange = function(from, to) {
+		if (from.x > to.x) {
+			this.setRange(to, from);
+			return;
+		}
+		// TODO
+		if (this.inBar(from) && this.inBar(to)) {
+			var fromX = this.location(from.x);
+			var toX = this.location(to.x);
+			//console.log(fromX, toX);
+			this.indicator.setRange(fromX, toX);
+		}
+	}
+
+	return new Loader();
+}($("#progressbar"));
+
+var startPoint = null;
+var dragging = false;
+function onMouseDrag(event) {
+	dragging = dragging || (Math.abs(event.point.x - startPoint.x) > 9);
+	if (dragging && Loader.inBar(event.point)) {
+		Loader.setRange(startPoint, event.point);
+	}
+}
+
+function onMouseDown(event) {
+	if (loader.inBar(event.point)) {
+		startPoint = event.point;
+		dragging = false;
+		loader.setPosition(event.point);
+	}
+}
+
+function onMouseUp(event) {
+	startPoint = null;
+
+}
