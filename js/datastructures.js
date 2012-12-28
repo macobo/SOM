bfa9b14249cbcd901e936745df436eeddaefa63a
macobo <oxymaccy@gmail.com>
2012-12-28 17:59:31 +0200

    Calculating next iterations

--- js/datastructures.js
+++ js/datastructures.js
@@ -15,332 +15,116 @@ function counterCallback(callback, count) {
 	});
 }
 
-(function() {
-	// Enum of states
-	var mapRadius = 500;
-	var iterations = 300;
-	var timeConstant = iterations / Math.log(mapRadius);
-	var learningConstant = 0.1;
-	window.states = Object.freeze({
-		NONE: 0,
-		CREATING_DATA: 1,
-		START_ITERATING: 2,
-		ITERATING: 3,
-		BROWSING: 4,
-	});
-
-	window.pathLayer = new Layer();
-	// Styles, used for configuration
-	// Path
-	pathStyle = {
-	    //strokeColor:"#60F",
-	    //strokeWidth: 2,
-	    //dashArray: [3, 3],
-	    //stokeCap: 'round',
-	    radius: 10
-	};
-	// Circle indicating current position
-	indicatorStyle = {
-	    fillColor: "#FFF", 
-	    strokeColor: "#000",
-	    radius: 10
-	};
-	// circle(s) denoting previous positions.
-	wayPostStyle = {
-		fillColor: "#777", 
-		opacity: 0.3,
-		//radius: 3,
-	};
-
-	var wayPost = function createWayPostSymbol() {
-	    var circle = new Path.Circle(new Point(0,0), wayPostStyle.radius);
-	    circle.style = wayPostStyle;
-	    circle.opacity = wayPostStyle.opacity;
-	    return new Symbol(circle);
-	}();
-
-	function NeuronPath(point) {
-		this.path = new Path();
-		this.path.style = pathStyle;
-		pathLayer.addChild(this.path);
-		this.pathGroup = new Group();
-		this.add(point);
-		this.indicator = new Path.Circle(point, indicatorStyle.radius);
-		this.indicator.style = indicatorStyle;
+var SOMConstats = {
+	radius: 500,
+	iterations: 500,
+	learning: 0.1,
+	influence: function(distance, radius) {
+		return Math.exp(-1.0 * distance * distance / (2 * radius * radius));
 	}
-
-	function memoizedPoint(curve, part) {
-		if (curve.cache === undefined) {
-			curve.cache = {};
-		}
-		var x = Number(part * 100);
-		if (!(curve.cache.hasOwnProperty(x))) 
-			curve.cache[x] = curve.getPoint(part);
-		return curve.getPoint(part);
+	neighborhoodRadius: function(iteration, constants) {
+		return constants.radius * Math.exp(-1.0 * iteration / constants.time);
 	}
-
-	// moves the indicator along some segment to part
-	NeuronPath.prototype.moveIndicator = function(segment, part) {
-		if (segment === undefined) {
-			segment = this.segmentCount() - 1;
-			part = 1;
-		}
-		if (this.segmentCount() == 0)
-			return;
-		var curve = this.path.curves[segment % this.segmentCount()];
-		this.indicator.position = memoizedPoint(curve, part);
+	learningRate: function(iteration, constants) {
+		return constants.learning * Math.exp(-1.0 * iteration / constants.time);
 	}
+}
+SOMConstats.time = SOMConstats.iterations / Math.log(SOMConstats.radius);
 
-	// change the color of the indicator
-	NeuronPath.prototype.setColor = function(newFillColor) {
-		this.indicator.fillColor = newFillColor;
-	}
-
-	NeuronPath.prototype.segmentCount = function() { return this.path.curves.length; }
-
-	// adds a point to path
-	NeuronPath.prototype.add = function(point) {
-		point = translate(point);
-		//console.log("adding to NeuronPath ",this, point);
-	    this.path.add(point);
-	    //this.path.smooth();
-	    // how to move them later
-	    this.pathGroup.addChild(
-	    	wayPost.place(point)
-	    );
-	}
-
-	// Neuron - datastructure containing the info about a neuron
-	function Neuron(startState) {
-		this.data = [];		
-		if (startState !== undefined)
-			this.addState(startState);
-	} 
-
-	_.extend(Neuron, {
+var NeuronStatus = {
 		DATA: {
 			indicator: {fillColor: "#FFF"},
 		},
 		DATASELECTED: {
-			indicator: {fillColor: "#00F"},
-			oneHolder: true
+			indicator: {fillColor: "#00F"}
 		},
 		NEUTRAL: {
 			indicator: {fillColor: "#090"},
 		},
 		BMU: {
-			indicator: {fillColor: "#F00"},
-			oneHolder: true,
+			indicator: {fillColor: "#F00"}
 		},
-		MATCH: {
+		INRANGE: {
 			indicator: {fillColor: "#FF0"},
 		},
-	});
+	}
 
-	Neuron.prototype.setStyle = function(style) {
-		if (style.indicator !== undefined) {
-			_.extend(this.path.indicator.style, style.indicator);
+Neuron = (function(constants, status) {
+	function Neuron() {
+		// Array of Point objects
+		this.segments = [];
+		this.status = [];
+	}
+
+	// If parameter is an array, it is first converted to Point
+	Neuron.prototype.addSegment = function(segment, status) {
+		if (Array.isArray(segment))
+			segment = new Point(segment);
+		this.segments.push(segment);
+		this.status.push(status)
+	}
+
+	Neuron.prototype.segmentCount = function() {
+		return this.segments.length;
+	}
+
+	// Returns a Point - the location of the neuron at that time (which should be a Number)
+	// If when is not provided, the last segment is given
+	Neuron.prototype.state = function(when) {
+		if (when === undefined)
+			when = this.segments.length-1;
+		return this.segments[when];
+	}
+
+
+	/*
+		STUFF related to calculating next states
+	*/
+	
+	var dimensions = ["x", "y"];
+	// Updates the neuron (by adding a new state at the end)
+	// Once updating is done, callback (if provided) is called
+	Neuron.prototype.updateState = function(vector, BMU, callback) {
+		var newSegment = this.state().clone();
+		var newStatus = status.NEUTRAL;
+
+		var iteration = this.segments.length;
+		var R = constants.neighborhoodRadius(iteration, constants);
+		var distance = BMU.state().getDistance(newSegment);
+		if (distance < R) {
+			var I = constants.influence(distance, R);
+			var L = constants.learningRate(iteration, constants);
+			_.each(dimensions, function(dim) {
+				newSegment[dim] += L * I * (vector[dim] - newSegment[dim]);
+			});
+			newStatus = (this == BMU) ? status.BMU : status.INRANGE;
 		}
-		this.prevStyle = style;
+		this.addSegment(newSegment, newStatus);
+		if (callback !== undefined)
+			callback();
 	}
 
-	// Gives a copy of value of data at the given moment.
-	// If no parameter is given, the last datapoint is returned.
-	Neuron.prototype.state = function(moment) {
-		if (moment === undefined)
-			moment = this.data.length-1;
-		return this.data[moment].slice();
-	} 
-
-	// Returns the neuron.
-	Neuron.prototype.addState = function(newState) {
-		var point = new Point(newState);
-		if (!Array.isArray(newState))
-			newState = [newState.x, newState.y];
-		if (this.path === undefined) {
-			this.path = new NeuronPath(point);
-		}
-		this.data.push(newState);
-	}
-
-	Neuron.prototype.updatePath = function() {
-		for (var i = this.path.segmentCount(); i < this.data.length; i++) {
-			var point = new Point(this.data[i]);
-			//console.log(this.data[i], point);
-			this.path.add(point);
-		}
-		this.path.moveIndicator(this.data.length-1, 1);
-	}
-
-	// Distance from current state (squared) to other neurons current state
-	Neuron.prototype.distance = function(neuron) {
-		var a = new Point(this.state());
-		if (neuron instanceof Neuron) {
-			var b = new Point(neuron.state());
-			return a.getDistance(b);
-		}
-		return neuron.getDistance(a);
-	}
-
-	// Calculates the influence over distance, with the neighborhood radius being radius.
-	function influence(distance, radius) {
-		return Math.exp(-1.0 * distance * distance / (2 * radius * radius));
-	}
-
-	// Calculates the neighborhood radius at given iteration
-	function neighborhoodRadius(iteration, timeConstant) {
-		// TODO: real mapRadius implementation?
-		return mapRadius * Math.exp(-1.0 * iteration / timeConstant);
-	}
-
-	// Calculates the learning rate at iteration
-	function learningRate(iteration, learningConstant, timeConstant) {
-		return learningConstant * Math.exp(-1.0 * iteration / timeConstant);
-	}
-
-	// Updates the neuron (by adding a new state)
-	Neuron.prototype.update = function(BMU, vector, iteration, callback) {
-		var R = neighborhoodRadius(iteration, timeConstant);
-		var distance = this.distance(BMU);
-		//if (distance > R) return;
-		
-		var L = learningRate(iteration, learningConstant, timeConstant);
-		var I = influence(distance, R);
-		// A prettier way to do it would be nice...
-		var state = this.state(); 
-
-		if (distance <= R) {
-			this.setStyle(Neuron.MATCH);
-			for (var i = 0; i < vector.length; i++)
-				state[i] += L * I * (vector[i] - state[i]);
-		} else {
-			this.setStyle(Neuron.NEUTRAL);
-		}
-		//console.log("Updating ", this, "adding new state ", state);
-		this.addState(state);
-		this.updatePath();
-		if (callback !== undefined) callback();
-	}
-
-	Neuron.findBMU = function(neurons, vector) {
-		var point = new Point(vector);
-		var closest = _.min(neurons, function(neuron) {
-			//console.log(neuron.data[0], vector, neuron.distance(point));
-			return neuron.distance(point); 
+	// Returns the closest neuron in the bunch
+	Neuron.prototype.findClosest = function(neurons) {
+		var point = this.state();
+		var closest = _.min(neurons, function (n) { 
+			point.getDistance(n.state()); 
 		});
-		//console.log(point, closest);
 		return closest;
 	}
 
-	// Generates new neurons within the box created by boxHighLeft, boxLowRight.
-	// If they are not provided, the size of canvas is used.
-	Neuron.generate = function(amount, boxHighLeft, boxLowRight) {
-		if (boxHighLeft === undefined) {
-			boxHighLeft = new Point(0, 0);
-			boxLowRight = new Point(canvas.width, canvas.height);
-		}
-		var size = boxLowRight - boxHighLeft;
-		// perhaps a generator pattern would have been better?
-		var neurons = []
-		for (var i = 0; i < amount; i++) {
-			var point = Point.random() * size + boxHighLeft;
-			neurons.push(new Neuron(point));
-		}
-		return neurons;
-	}
-
-	var iteration = 0;
-	Neuron.nextIteration = function(data, neurons, callback) {
-		_.each(data, function(d) { d.setStyle(Neuron.DATA) });
-		var vector = selectRandom(data);
-		vector.setStyle(Neuron.NEUTRAL);
-		//console.log("Data vector", vector.state());
-		//Neuron.updateAll(neurons, vector.state(), iteration);
-		var BMU = Neuron.findBMU(neurons, vector);
-		//console.log("BMU: ", BMU);
-		var doneCallback = counterCallback(function() {
-			iteration += 1;
-			console.log(iteration);
-			infoText.content = "iteration: "+(iteration+1);
-			callback();
-		}, neurons.length);
-		_.each(neurons, function(neuron) { 
-			
-			neuron.update(
-				BMU, 
-				vector.state(),
-				iteration,
-				doneCallback
-			); 
+	// Generates the next iteration from current state.
+	// If vector is not supplied as argument, a random vector from data is used.
+	Neuron.nextIteration = function(data, neurons, vector) {
+		if (vector === undefined)
+			vector = selectRandom(data);
+		var BMU = vector.findClosest(neurons);
+		// Make sure we call the callback once we are done with all the neurons
+		var counter = counterCallback(callback, this.state.length);
+		_.each(neurons, function(neuron) {
+			neuron.updateState(vector, BMU, counter);
 		});
-		BMU.setStyle(Neuron.BMU);
 	}
 
-	var infoText = new PointText(new Point(0,20))
-	infoText.characterStyle = {
-	    fontSize: 20,
-	    fillColor: 'black',
-	};
 
-	window.NeuronPath = NeuronPath;
-	window.Neuron = Neuron;
-	window.infoText = infoText;
-})();
-
-var state = states.CREATING_DATA;
-data = [];
-neurons = [];
-
-tool.minDistance = 10;
-function onMouseDrag(event) {
-	if (state == states.CREATING_DATA) {
-		data.push(new Neuron(event.point));
-	}
-}
-var lastTime = 0, x = 0, target = 0;
-var timeframe = 1;
-function onMouseDown(event) {
-	if (state == states.CREATING_DATA) {
-		data.push(new Neuron(event.point));
-	}
-}
-function onFrame(event) {
-	//console.log(event);
-}
-
-function onKeyDown(event) {
-	if (state == states.CREATING_DATA && data.length > 10) {
-		state = states.START_ITERATING;
-		neurons = Neuron.generate(100);
-	} else if (state == states.START_ITERATING) {
-		var iterations = 300;
-		var callback = function() {
-			iterations--;
-			if (iterations > 0) 
-				setTimeout(function() { 
-					Neuron.nextIteration(data, neurons, callback); 
-				}, 10);
-		}
-		callback();
-		state = states.ITERATING;
-	}
-}
-
-
-/*
-The plan:
-Each "NeuronPath" is an object containing the path of this NeuronPath as well as the circle of this NeuronPath.
-In the same scope exists the settings, etcetc. (also the current position of the circle)
-
-There's a method move(value) which appends a new value at end, updates, smoothes
-There's a method drawCircle(segment, at) (with at in [0..1]) 
-which changes the current position of the circle to appropriate place (used for drawing)
-
-There's also a method changeColor
-(and perhaps a changeState method that uses premade configuration/enum to change colors automatically)
-
-Ideas: instead of using raw canvas x,y coordinates, there should be a method to translate absolute coordinates
-used within logic to coordinates shown in canvas at the moment (used for scaling, etc).
-	Global method translate(Point), scaled(size)
-If so, there's a need for a update method which updates the path as needed.
-*/
+})(SOMConstats, NeuronStatus);
