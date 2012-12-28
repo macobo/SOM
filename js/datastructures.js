d98d9b7f87606a74a53fed4284800327ae6a6d26
macobo <oxymaccy@gmail.com>
2012-12-28 21:38:51 +0200

    Path drawing tested

--- js/datastructures.js
+++ js/datastructures.js
@@ -1,37 +1,43 @@
 // TODO: PaperPath should be generated on demand, data constructor
 
-Neuron = (function(constants, status, styles) {
-	function Neuron(segment) {
+Neuron = (function(constants, statuses, styles) {
+	function Neuron() {
 		// Array of Point objects
-		this.segments = [];
-		this.status = [];
+		this._segments = [];
+		this._status = [];
 		this.path = new Path();
 		this.path.style = styles.path;
 		this.indicator = new Path.Circle(new Point(0, 0), styles.indicator.radius);
 		this.indicator.style = styles.indicator;
-		this.indicator.visible = true;
-		if (segment !== undefined)
-			this.addSegment(segment, status.DATA);
+		this.indicator.visible = false;
 	}
 
 	// If parameter is an array, it is first converted to Point
 	Neuron.prototype.addSegment = function(segment, status) {
 		if (Array.isArray(segment))
 			segment = new Point(segment);
-		this.segments.push(segment);
-		this.status.push(status)
+		if (status === undefined)
+			status = statuses.NEUTRAL;
+		this._segments.push(segment);
+		this._status.push(status)
 	}
 
 	Neuron.prototype.segmentCount = function() {
-		return this.segments.length;
+		return this._segments.length;
 	}
 
 	// Returns a Point - the location of the neuron at that time (which should be a Number)
 	// If when is not provided, the last segment is given
-	Neuron.prototype.state = function(when) {
+	Neuron.prototype.segment = function(when) {
 		if (when === undefined)
-			when = this.segments.length-1;
-		return this.segments[when];
+			when = this._segments.length-1;
+		return this._segments[when];
+	}
+
+	Neuron.prototype.status = function(when) {
+		if (when === undefined)
+			when = this._segments.length-1;
+		return this._status[when];
 	}
 
 
@@ -43,19 +49,19 @@ Neuron = (function(constants, status, styles) {
 	// Updates the neuron (by adding a new state at the end)
 	// Once updating is done, callback (if provided) is called
 	Neuron.prototype.updateState = function(vector, BMU, callback) {
-		var newSegment = this.state().clone();
-		var newStatus = status.NEUTRAL;
+		var newSegment = this.segment().clone();
+		var newStatus = statuses.NEUTRAL;
 
-		var iteration = this.segments.length;
+		var iteration = this.segmentCount();
 		var R = constants.neighborhoodRadius(iteration, constants);
-		var distance = BMU.state().getDistance(newSegment);
+		var distance = BMU.segment().getDistance(newSegment);
 		if (distance < R) {
 			var I = constants.influence(distance, R);
 			var L = constants.learningRate(iteration, constants);
 			_.each(dimensions, function(dim) {
 				newSegment[dim] += L * I * (vector[dim] - newSegment[dim]);
 			});
-			newStatus = (this == BMU) ? status.BMU : status.INRANGE;
+			newStatus = (this == BMU) ? statuses.BMU : statuses.INRANGE;
 		}
 		this.addSegment(newSegment, newStatus);
 		if (callback !== undefined)
@@ -64,9 +70,9 @@ Neuron = (function(constants, status, styles) {
 
 	// Returns the closest neuron in the bunch
 	Neuron.prototype.findClosest = function(neurons) {
-		var point = this.state();
+		var point = this.segment();
 		var closest = _.min(neurons, function (n) { 
-			point.getDistance(n.state()); 
+			point.getDistance(n.segment()); 
 		});
 		return closest;
 	}
@@ -78,7 +84,7 @@ Neuron = (function(constants, status, styles) {
 			vector = selectRandom(data);
 		var BMU = vector.findClosest(neurons);
 		// Make sure we call the callback once we are done with all the neurons
-		var counter = counterCallback(callback, this.state.length);
+		var counter = counterCallback(callback, neurons.length);
 		_.each(neurons, function(neuron) {
 			neuron.updateState(vector, BMU, counter);
 		});
@@ -89,28 +95,11 @@ Neuron = (function(constants, status, styles) {
 	*/
 	// Builds (if neccesary) the path from [from -> to] (inclusive)
 	Neuron.prototype.showPath = function(from, to) {
-		// nothing in the path or no overlap
-		if (this.path.segments.length == 0 || this.prevTo < from || this.prevFrom > to) {
-			this.path.removeChildren();
-			this.prevTo = to;
-			this.prevFrom = from;
-			this.path.addSegments(this.segments.slice(from, to+1));
-			return;
-		}
-		// remove from end
-		if (to < this.prevTo)
-			this.path.removeChildren(to-this.prevFrom);
-		// append
-		else
-			this.path.addSegments(this.segments.slice(this.prevTo+1, to+1));
-		// remove from start
-		if (from > this.prevFrom)
-			this.path.removeChildren(0, from-this.prevFrom);
-		// prepend
-		else
-			this.path.addSegments(this.segments.slice(from, this.prevFrom));
-		this.prevTo = to;
-		this.prevFrom = from;
+		this.path.removeSegments();
+		this.path.addSegments(this._segments.slice(from, to+1));
+		this.path.currentStart = from;
+		this.path.currentEnd = to;
+		this.path.smooth();
 	}
 
 	Neuron.showPaths = function(neurons, from, to) {
@@ -121,5 +110,21 @@ Neuron = (function(constants, status, styles) {
 
 	// TODO: functions to toggle the visibility of parts
 
-	return Neuron
-})(SOMConstants, NeuronStatus, PathStyles);
+	Neuron.prototype.getCurve = function(segment) {
+		return this.path.curves[segment - this.path.currentStart];
+	}
+
+	// If segment is not provided, the last (drawn) segment is assumed
+	// if part is not given, 1 is used
+	Neuron.prototype.setIndicator = function(segment, part) {
+		if (segment === undefined) {
+			segment = this.path.currentEnd-1;
+			part = 1;
+		}
+		_.extend(this.indicator.style, this._status[segment+1]);
+		this.indicator.position = this.getCurve(segment).getPoint(part);
+		this.indicator.visible = true;
+	}
+
+	return Neuron;
+})(SOMConstants, NeuronStatus, PathStyles);	
--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,15 @@
-// This is where the drawing logic is. :)
+// This is where the magic happens. :)
+
+function onFrame() {
+
+}
+
+a = new Neuron();
+a.addSegment(new Point(100, 100));
+a.addSegment(new Point(50, 150));
+a.addSegment(new Point(100, 200));
+a.addSegment(new Point(150, 150));
+a.addSegment(new Point(300, 150));
+a.showPath(0, 2);
+a.showPath(1, 4);
+a.setIndicator(3, 0.5);
